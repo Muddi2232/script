bc9fe676889277905207d837347e41675ec804bd
Muddi2232 <154959246+Muddi2232@users.noreply.github.com>
2024-02-29 00:22:24 +0500

    jssss

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-alert("Hello World")
+ alert("Hello World")
+var age = 20;
+ var fullName = 'MuddassirKhalid';
+ console.log(fullName)
+ console.log(age)
+
+
+// Post Increment
+var num= 2
+var newNum= num++
+console.log('num ->', num)
+console.log('newNum->', newNum)
+
+// Pre increment
+
+var pre=2
+var preNum= ++pre
+console.log('pre->', pre)
+console.log('preNum->', preNum)
